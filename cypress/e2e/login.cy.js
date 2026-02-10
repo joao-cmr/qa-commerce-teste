0924f010b380a536cc1456a3c58cfac7b18f69b1
joao-cmr <contatojoaocmr@gmail.com>
2026-02-10 18:55:58 -0300

    HOOKS

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,39 @@
-
+/// <reference types="cypress" />
 
 describe('Teste de funcionalidade login', () => {
+
+  //HOOKS/Ganchos
+
+  //Antes de cada teste
+beforeEach(() => {
+  cy.visit('login.html')
+});
+
+  //Depois de cada teste
+afterEach(() => {
+  cy.screenshot()
+});
+  
+  
+  
+
   it('Deve fazer login com sucesso', () => {
-    cy.visit('http://localhost:3000/login.html')
     cy.get('#email').type('admin@admin.com')
     cy.get('#password').type('admin')
     cy.get('.btn').click()
+    cy.get('h1').should('contain', 'Minha conta')
 
-    //falta validar o resultado esperado
+    
   })
 
   it('Deve fazer logout com sucesso', () => {
-    cy.visit('http://localhost:3000/login.html')
     cy.get('#email').type('admin@admin.com')
     cy.get('#password').type('admin')
     cy.get('.btn').click()
     cy.get('#logout-button').click()
+    cy.url().should('include', '/login.html')
 
-    //Clicar no botão logout
-    //validar o resultado esperado
+    
   });
 
   
