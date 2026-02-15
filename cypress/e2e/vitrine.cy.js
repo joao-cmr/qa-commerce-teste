f137185effb16eb8d7174035128cf80949e8e012
joao-cmr <contatojoaocmr@gmail.com>
2026-02-15 10:18:48 -0300

    Fixture Com Array de objetos

--- cypress/e2e/vitrine.cy.js
+++ cypress/e2e/vitrine.cy.js
@@ -8,7 +8,7 @@ describe('Testes da funcionalidade vitrine', () => {
     });
 
     it('Deve buscar um produto pelo nome', () => {
-       
+
         cy.contains('Moletom com capuz').click()
         cy.get('.btn-secondary').should('exist')
     });
@@ -16,27 +16,37 @@ describe('Testes da funcionalidade vitrine', () => {
     it('Deve clicar no primeiro produto da lista', () => {
         cy.get('.card-img-top').first().click()
         cy.url().should('include', 'product.html')
-        
+
     });
 
-     it('Deve clicar no ultimo produto da lista', () => {
+    it('Deve clicar no ultimo produto da lista', () => {
         cy.get('.card-img-top').last().click()
         cy.url().should('include', 'product.html')
         cy.get('legend').should('contain', 'Moletom com capuz "Const"')
-        
+
     });
 
     it('Deve clicar no terceiro produto da lista', () => {
         cy.get('.card-img-top').eq(2).click()
         cy.get('legend').should('contain', 'Ecobag "Na minha máquina funciona"')
-        
+
     });
 
     it('Deve buscar um produto usando massa da dados', () => {
         cy.fixture('produto-unico').then((produto => {
-        cy.contains(produto.Produto.nome).click()
-        cy.get('#product-price').should('contain', produto.Produto.preco)
+            cy.contains(produto.Produto.nome).click()
+            cy.get('#product-price').should('contain', produto.Produto.preco)
         }))
     });
-        
+
+    it.only('Deve buscar produtos usando massa de dados múltiplos', () => {
+        cy.fixture('multiplos-produtos').then((produtos) => {
+            produtos.forEach((prod) => {
+                cy.contains(prod.Produto.nome).click()
+                cy.get('#product-price').should('contain', prod.Produto.preco)
+                cy.go('back')
+            })
+        })
+    });
+
 }); 
