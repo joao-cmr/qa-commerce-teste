a41403534a96fe9ee3337092d4917a7126282b6d
joao-cmr <contatojoaocmr@gmail.com>
2026-02-16 14:14:24 -0300

    Usando massa de dados dinâmica com Faker

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import  {  faker  }  from  '@faker-js/faker' ;
 
 describe('Testes da funcionalidade de checkout', () => {
 
@@ -6,14 +7,17 @@ describe('Testes da funcionalidade de checkout', () => {
         cy.visit('checkout.html');
     });
 
-    it.only('Deve preencher o formulário de checkout com sucesso', () => {
+    it('Deve preencher o formulário de checkout com sucesso', () => {
+
+        var email = `teste${Date.now()}@gmail.com.br`;
+
         cy.get('#first-name').type('João Carlos');
         cy.get('#last-name').type('M. Rodrigues');
         cy.get('#address').type('Rua Iperó')
         cy.get('#number').type('26');
         cy.get('#cep').type('07793365');
         cy.get('#phone').type('11972641956');
-        cy.get('#email').type('teste6@gmail.com.br');
+        cy.get('#email').type(email);
         cy.get('#create-account').click();
         cy.get('#password').type('180524Jc*');
         cy.get('#confirm-password').type('180524Jc*');
@@ -22,6 +26,25 @@ describe('Testes da funcionalidade de checkout', () => {
         cy.get('.btn').click();
         cy.get('h1').should('contain', 'STATUS DO PEDIDO');
 
+    });
+
+    it('Deve fazer checkout com sucesso - usando faker - sem otimização', () => {
+        cy.get('#first-name').type(faker.person.firstName());
+        cy.get('#last-name').type(faker.person.lastName());
+        cy.get('#address').type('Rua Iperó')
+        cy.get('#number').type('26');
+        cy.get('#cep').type('07793365');
+        cy.get('#phone').type('11972641956');
+        cy.get('#email').type(faker.internet.email());
+        cy.get('#create-account').click();
+        cy.get('#password').type('180524Jc*');
+        cy.get('#confirm-password').type('180524Jc*');
+        cy.get('#payment-boleto').click();
+        cy.get('#terms').check();
+        cy.get('.btn').click();
+        cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+
+        
     });
 })
 
