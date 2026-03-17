60d94878a1f8bef6f7a2ea505b81d949b4ef8d79
joao-cmr <contatojoaocmr@gmail.com>
2026-03-17 13:09:52 -0300

    Correção no teste de vitrine para github actions

--- cypress/e2e/vitrine.cy.js
+++ cypress/e2e/vitrine.cy.js
@@ -1,52 +1,49 @@
 /// <reference types="cypress" />
 
-
 describe('Testes da funcionalidade vitrine', () => {
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('/');
+        cy.get('.card-img-top', { timeout: 10000 }).should('be.visible');
     });
 
     it('Deve buscar um produto pelo nome', () => {
-
-        cy.contains('Moletom com capuz').click()
-        cy.get('.btn-secondary').should('exist')
+        cy.contains('Moletom com capuz', { timeout: 10000 }).should('be.visible').click();
+        cy.get('.btn-secondary').should('exist');
     });
 
     it('Deve clicar no primeiro produto da lista', () => {
-        cy.get('.card-img-top').first().click()
-        cy.url().should('include', 'product.html')
-
+        cy.get('.card-img-top').first().click();
+        cy.url().should('include', 'product.html');
     });
 
     it('Deve clicar no ultimo produto da lista', () => {
-        cy.get('.card-img-top').last().click()
-        cy.url().should('include', 'product.html')
-        cy.get('legend').should('contain', 'Moletom com capuz "Const"')
-
+        cy.get('.card-img-top').last().click();
+        cy.url().should('include', 'product.html');
+        cy.get('legend', { timeout: 10000 }).should('contain', 'Moletom com capuz "Const"');
     });
 
     it('Deve clicar no terceiro produto da lista', () => {
-        cy.get('.card-img-top').eq(2).click()
-        cy.get('legend').should('contain', 'Ecobag "Na minha máquina funciona"')
-
+        cy.get('.card-img-top').eq(2).click();
+        cy.get('legend', { timeout: 10000 }).should('contain', 'Ecobag "Na minha máquina funciona"');
     });
 
     it('Deve buscar um produto usando massa da dados', () => {
-        cy.fixture('produto-unico').then((produto => {
-            cy.contains(produto.Produto.nome).click()
-            cy.get('#product-price').should('contain', produto.Produto.preco)
-        }))
+        cy.fixture('produto-unico').then((produto) => {
+            cy.contains(produto.Produto.nome, { timeout: 10000 }).should('be.visible').click();
+            cy.get('#product-price', { timeout: 10000 }).should('contain', produto.Produto.preco);
+        });
     });
 
     it('Deve buscar produtos usando massa de dados múltiplos', () => {
         cy.fixture('multiplos-produtos').then((produtos) => {
             produtos.forEach((prod) => {
-                cy.contains(prod.Produto.nome).click()
-                cy.get('#product-price').should('contain', prod.Produto.preco)
-                cy.go('back')
-            })
-        })
+                cy.contains(prod.Produto.nome, { timeout: 10000 }).should('be.visible').click();
+                cy.get('#product-price', { timeout: 10000 }).should('contain', prod.Produto.preco);
+                cy.go('back');
+                cy.get('.card-img-top', { timeout: 10000 }).should('be.visible');
+            });
+        });
     });
 
-}); 
+});
