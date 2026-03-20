457c844b1726f5e2f2683b0b73019ba9aafff591
joao-cmr <contatojoaocmr@gmail.com>
2026-03-20 19:04:27 -0300

    Checkout
    Aumentando a cobertura de testes

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -87,104 +87,233 @@ describe('Testes da funcionalidade de checkout', () => {
             cy.get('.btn').click();
             cy.get('h1').should('contain', 'STATUS DO PEDIDO');
         });
+
+        
+        it('Deve fazer checkout com sucesso - pagamento via Pix', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            cy.get('#create-account').click();
+            cy.get('#password').type('180524Jc*');
+            cy.get('#confirm-password').type('180524Jc*');
+            cy.get('#payment-pix').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+        });
+
+        
+        it('Deve fazer checkout como visitante - sem criar conta', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+        });
     });
 
     describe('Validações de Erro - Prioridade Crítica (P0)', () => {
 
-    it('Não deve permitir finalizar checkout com campos obrigatórios vazios', () => {
-        cy.get('.btn').click();
-        cy.contains('Você deve concordar com os termos e condições').should('be.visible');
-        cy.contains('Este campo é obrigatório.').should('be.visible');
-    });
+        it('Não deve permitir finalizar checkout com campos obrigatórios vazios', () => {
+            cy.get('.btn').click();
+            cy.contains('Você deve concordar com os termos e condições').should('be.visible');
+            cy.contains('Este campo é obrigatório.').should('be.visible');
+        });
 
-    it('Não deve permitir finalizar checkout sem aceitar os termos', () => {
-        checkoutPage.preencherFormularioCheckout(
-            faker.person.firstName(),
-            faker.person.lastName(),
-            'Rua Iperó',
-            '26',
-            '07793365',
-            '11972641956',
-            faker.internet.email()
-        );
-        cy.get('#payment-boleto').click();
-        cy.get('.btn').click();
-        cy.contains('Você deve concordar com os termos e condições').should('be.visible');
-    });
+        it('Não deve permitir finalizar checkout sem aceitar os termos', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            cy.get('#payment-boleto').click();
+            cy.get('.btn').click();
+            cy.contains('Você deve concordar com os termos e condições').should('be.visible');
+        });
 
-    it('Não deve permitir criar conta quando senhas não conferem', () => {
-        checkoutPage.preencherFormularioCheckout(
-            faker.person.firstName(),
-            faker.person.lastName(),
-            'Rua Iperó',
-            '26',
-            '07793365',
-            '11972641956',
-            faker.internet.email()
-        );
-        cy.get('#create-account').click();
-        cy.get('#password').type('180524Jc*');
-        cy.get('#confirm-password').type('SenhaDiferente123*');
-        cy.get('#payment-boleto').click();
-        cy.get('#terms').check();
-        cy.get('.btn').click();
-        cy.contains('As senhas não coincidem').should('be.visible');
-    });
+        it('Não deve permitir criar conta quando senhas não conferem', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            cy.get('#create-account').click();
+            cy.get('#password').type('180524Jc*');
+            cy.get('#confirm-password').type('SenhaDiferente123*');
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.contains('As senhas não coincidem').should('be.visible');
+        });
 
-    it.skip('Não deve processar pagamento com número de cartão inválido', () => {
-        checkoutPage.preencherFormularioCheckout(
-            faker.person.firstName(),
-            faker.person.lastName(),
-            'Rua Iperó',
-            '26',
-            '07793365',
-            '11972641956',
-            faker.internet.email()
-        );
-        checkoutPage.pagamentoCard(
-            '1234567890123456',
-            '05/2025',
-            '123'
-        );
-
-        cy.get('#terms').check();
-        cy.get('.btn').click();
-        cy.screenshot('cartao-invalido');
-        cy.url().should('include', 'checkout.html');
+        // TODO: Remover .skip() após correção - Issue #1
+        it.skip('Não deve processar pagamento com número de cartão inválido', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            checkoutPage.pagamentoCard(
+                '1234567890123456',
+                '05/2025',
+                '123'
+            );
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.url().should('include', 'checkout.html');
+        });
     });
-});
 
     describe('Validações de Erro - Prioridade Alta (P1)', () => {
 
-    it('Não deve aceitar email com formato inválido', () => {
-        cy.get('#first-name').type(faker.person.firstName());
-        cy.get('#last-name').type(faker.person.lastName());
-        cy.get('#address').type('Rua Iperó');
-        cy.get('#number').type('26');
-        cy.get('#cep').type('07793365');
-        cy.get('#phone').type('11972641956');
-        cy.get('#email').type('emailinvalido.com');
-        cy.get('#payment-boleto').click();
-        cy.get('#terms').check();
-        cy.get('.btn').click();
-        cy.contains('Por favor, insira um email válido').should('be.visible');
+        it('Não deve aceitar email com formato inválido', () => {
+            cy.get('#first-name').type(faker.person.firstName());
+            cy.get('#last-name').type(faker.person.lastName());
+            cy.get('#address').type('Rua Iperó');
+            cy.get('#number').type('26');
+            cy.get('#cep').type('07793365');
+            cy.get('#phone').type('11972641956');
+            cy.get('#email').type('emailinvalido.com');
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.contains('Por favor, insira um email válido').should('be.visible');
+        });
+
+        it('Não deve aceitar CEP inválido', () => {
+            cy.get('#first-name').type(faker.person.firstName());
+            cy.get('#last-name').type(faker.person.lastName());
+            cy.get('#address').type('Rua Iperó');
+            cy.get('#number').type('26');
+            cy.get('#cep').type('123');
+            cy.get('#phone').type('11972641956');
+            cy.get('#email').type(faker.internet.email());
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.contains('O CEP deve ter 8 caracteres').should('be.visible');
+        });
+
+        // TODO: Remover .skip() após correção - Issue #2
+        it.skip('Não deve processar cartão com data de validade expirada', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            checkoutPage.pagamentoCard(
+                '4532123456789012',
+                '05/2020',
+                '123'
+            );
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.url().should('include', 'checkout.html');
+        });
+
+        // TODO: Remover .skip() após correção - Issue #3
+        it.skip('Não deve processar cartão com CVV inválido', () => {
+            checkoutPage.preencherFormularioCheckout(
+                faker.person.firstName(),
+                faker.person.lastName(),
+                'Rua Iperó',
+                '26',
+                '07793365',
+                '11972641956',
+                faker.internet.email()
+            );
+            checkoutPage.pagamentoCard(
+                '4532123456789012',
+                '05/2025',
+                '12'
+            );
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.url().should('include', 'checkout.html');
+        });
     });
 
-    it('Não deve aceitar CEP inválido', () => {
-        cy.get('#first-name').type(faker.person.firstName());
-        cy.get('#last-name').type(faker.person.lastName());
-        cy.get('#address').type('Rua Iperó');
-        cy.get('#number').type('26');
-        cy.get('#cep').type('123'); // CEP incompleto
-        cy.get('#phone').type('11972641956');
-        cy.get('#email').type(faker.internet.email());
-        cy.get('#payment-boleto').click();
-        cy.get('#terms').check();
-        cy.get('.btn').click();
-        cy.contains('O CEP deve ter 8 caracteres').should('be.visible');
-    });
+    describe('Testes de Segurança', () => {
 
-    it.skip('Não deve processar cartão com data de validade expirada', () => {
+        it('Não deve permitir SQL Injection no campo de nome', () => {
+            cy.get('#first-name').type("'; DROP TABLE users; --");
+            cy.get('#last-name').type(faker.person.lastName());
+            cy.get('#address').type('Rua Iperó');
+            cy.get('#number').type('26');
+            cy.get('#cep').type('07793365');
+            cy.get('#phone').type('11972641956');
+            cy.get('#email').type(faker.internet.email());
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+        });
+
+        it('Não deve permitir XSS no campo de endereço', () => {
+            cy.get('#first-name').type(faker.person.firstName());
+            cy.get('#last-name').type(faker.person.lastName());
+            cy.get('#address').type('<script>alert("XSS")</script>');
+            cy.get('#number').type('26');
+            cy.get('#cep').type('07793365');
+            cy.get('#phone').type('11972641956');
+            cy.get('#email').type(faker.internet.email());
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+            cy.on('window:alert', () => {
+                throw new Error('XSS detectado! Alert foi executado.');
+            });
+        });
+
+        it('Não deve permitir caracteres especiais maliciosos no email', () => {
+            cy.get('#first-name').type(faker.person.firstName());
+            cy.get('#last-name').type(faker.person.lastName());
+            cy.get('#address').type('Rua Iperó');
+            cy.get('#number').type('26');
+            cy.get('#cep').type('07793365');
+            cy.get('#phone').type('11972641956');
+            cy.get('#email').type('"><script>alert(1)</script>@test.com');
+            cy.get('#payment-boleto').click();
+            cy.get('#terms').check();
+            cy.get('.btn').click();
+            cy.contains('"email" must be a valid email').should('be.visible');
+    });
+});
+
+describe('Testes de Usabilidade', () => {
+
+    it('Deve alternar entre formas de pagamento - Boleto para Cartão', () => {
         checkoutPage.preencherFormularioCheckout(
             faker.person.firstName(),
             faker.person.lastName(),
@@ -194,18 +323,18 @@ describe('Testes da funcionalidade de checkout', () => {
             '11972641956',
             faker.internet.email()
         );
-        checkoutPage.pagamentoCard(
-            '4532123456789012',
-            '05/2020', 
-            '123'
-        );
+        cy.get('#payment-boleto').click();
+        cy.get('#payment-boleto').should('be.checked');
+        cy.get('#payment-card').click();
+        cy.get('#payment-card').should('be.checked');
+        cy.get('#payment-boleto').should('not.be.checked');
+        checkoutPage.pagamentoCard('4532123456789012', '05/2025', '123');
         cy.get('#terms').check();
         cy.get('.btn').click();
-        cy.screenshot('cartao-expirado');
-        cy.url().should('include', 'checkout.html');
+        cy.get('h1').should('contain', 'STATUS DO PEDIDO');
     });
 
-    it.skip('Não deve processar cartão com CVV inválido', () => {
+    it('Deve alternar entre formas de pagamento - Cartão para Pix', () => {
         checkoutPage.preencherFormularioCheckout(
             faker.person.firstName(),
             faker.person.lastName(),
@@ -215,15 +344,39 @@ describe('Testes da funcionalidade de checkout', () => {
             '11972641956',
             faker.internet.email()
         );
-        checkoutPage.pagamentoCard(
-            '4532123456789012',
-            '05/2025',
-            '12' 
-        );
+
+        
+        cy.get('#payment-card').click();
+        cy.get('#payment-card').should('be.checked');
+        cy.get('#payment-pix').click();
+        cy.get('#payment-pix').should('be.checked');
+        cy.get('#payment-card').should('not.be.checked');
+
         cy.get('#terms').check();
         cy.get('.btn').click();
-        cy.screenshot('cvv-invalido');
-        cy.url().should('include', 'checkout.html');
+        cy.get('h1').should('contain', 'STATUS DO PEDIDO');
+    });
+
+    it('Deve alternar entre formas de pagamento - Pix para Boleto', () => {
+        checkoutPage.preencherFormularioCheckout(
+            faker.person.firstName(),
+            faker.person.lastName(),
+            'Rua Iperó',
+            '26',
+            '07793365',
+            '11972641956',
+            faker.internet.email()
+        );
+
+       
+        cy.get('#payment-pix').click();
+        cy.get('#payment-pix').should('be.checked');
+        cy.get('#payment-boleto').click();
+        cy.get('#payment-boleto').should('be.checked');
+        cy.get('#payment-pix').should('not.be.checked');
+        cy.get('#terms').check();
+        cy.get('.btn').click();
+        cy.get('h1').should('contain', 'STATUS DO PEDIDO');
     });
 });
 });
--- docs/CASOS_TESTE_CHECKOUT.md
+++ docs/CASOS_TESTE_CHECKOUT.md
@@ -107,9 +107,42 @@ E devo visualizar o título "STATUS DO PEDIDO"
 
 ---
 
+#### **Cenário 5:** Finalizar checkout com pagamento via Pix
+```gherkin
+Dado que estou na página de checkout
+Quando eu preencho o formulário usando Page Object
+E marco a opção "Deseja criar uma conta"
+E preencho senha e confirmação de senha
+E seleciono a forma de pagamento "Pix"
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo ser redirecionado para a página de status do pedido
+E devo visualizar o título "STATUS DO PEDIDO"
+```
+
+**Objetivo:** Validar fluxo completo de pagamento com Pix (terceira forma de pagamento disponível).
+
+---
+
+#### **Cenário 6:** Finalizar checkout como visitante (sem criar conta)
+```gherkin
+Dado que estou na página de checkout
+Quando eu preencho o formulário usando Page Object
+E NÃO marco a opção "Deseja criar uma conta"
+E seleciono a forma de pagamento "Boleto"
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo ser redirecionado para a página de status do pedido
+E devo visualizar o título "STATUS DO PEDIDO"
+```
+
+**Objetivo:** Validar que usuários podem finalizar compra sem criar conta (checkout como visitante).
+
+---
+
 ##  Validações de Erro - Prioridade Crítica (P0)
 
-#### **Cenário 5:** Não deve permitir finalizar checkout com campos obrigatórios vazios
+#### **Cenário 7:** Não deve permitir finalizar checkout com campos obrigatórios vazios
 ```gherkin
 Dado que estou na página de checkout
 Quando eu clico em "Finalizar Pedido" sem preencher nenhum campo
@@ -122,7 +155,7 @@ E não devo ser redirecionado para a página de status
 
 ---
 
-#### **Cenário 6:** Não deve permitir finalizar checkout sem aceitar os termos
+#### **Cenário 8:** Não deve permitir finalizar checkout sem aceitar os termos
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios corretamente
@@ -137,7 +170,7 @@ E não devo ser redirecionado para a página de status
 
 ---
 
-#### **Cenário 7:** Não deve permitir criar conta quando senhas não conferem
+#### **Cenário 9:** Não deve permitir criar conta quando senhas não conferem
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios corretamente
@@ -155,7 +188,7 @@ E não devo ser redirecionado para a página de status
 
 ---
 
-#### **Cenário 8:** Não deve processar pagamento com número de cartão inválido  **[BUG - Issue #1]**
+#### **Cenário 10:** Não deve processar pagamento com número de cartão inválido  **[BUG - Issue #1]**
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios corretamente
@@ -182,7 +215,7 @@ Mas atualmente:
 
 ##  Validações de Erro - Prioridade Alta (P1)
 
-#### **Cenário 9:** Não deve aceitar email com formato inválido
+#### **Cenário 11:** Não deve aceitar email com formato inválido
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios
@@ -198,7 +231,7 @@ E não devo ser redirecionado para a página de status
 
 ---
 
-#### **Cenário 10:** Não deve aceitar CEP inválido
+#### **Cenário 12:** Não deve aceitar CEP inválido
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios
@@ -214,7 +247,7 @@ E não devo ser redirecionado para a página de status
 
 ---
 
-#### **Cenário 11:** Não deve processar cartão com data de validade expirada  **[BUG - Issue #2]**
+#### **Cenário 13:** Não deve processar cartão com data de validade expirada  **[BUG - Issue #2]**
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios corretamente
@@ -239,7 +272,7 @@ Mas atualmente:
 
 ---
 
-#### **Cenário 12:** Não deve processar cartão com CVV inválido  **[BUG - Issue #3]**
+#### **Cenário 14:** Não deve processar cartão com CVV inválido  **[BUG - Issue #3]**
 ```gherkin
 Dado que estou na página de checkout
 Quando eu preencho todos os campos obrigatórios corretamente
@@ -264,6 +297,117 @@ Mas atualmente:
 
 ---
 
+##  Testes de Segurança
+
+#### **Cenário 15:** Sistema deve sanitizar entrada de SQL Injection no campo nome
+```gherkin
+Dado que estou na página de checkout
+Quando eu preencho o campo nome com string maliciosa: "'; DROP TABLE users; --"
+E preencho os demais campos corretamente
+E seleciono a forma de pagamento "Boleto"
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então o sistema deve processar o pedido normalmente (sanitizando o input)
+E devo ser redirecionado para a página de status
+E devo visualizar o título "STATUS DO PEDIDO"
+```
+
+**Objetivo:** Validar que o sistema protege contra SQL Injection (sanitização de entrada).
+
+---
+
+#### **Cenário 16:** Sistema não deve executar scripts XSS no campo endereço
+```gherkin
+Dado que estou na página de checkout
+Quando eu preencho o campo endereço com script malicioso: "<script>alert('XSS')</script>"
+E preencho os demais campos corretamente
+E seleciono a forma de pagamento "Boleto"
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então o sistema não deve executar o script
+E devo ser redirecionado para a página de status
+E devo visualizar o título "STATUS DO PEDIDO"
+E não deve aparecer nenhum alert na tela
+```
+
+**Objetivo:** Validar proteção contra Cross-Site Scripting (XSS).
+
+---
+
+#### **Cenário 17:** Sistema deve rejeitar email com caracteres especiais maliciosos
+```gherkin
+Dado que estou na página de checkout
+Quando eu preencho o campo email com formato malicioso: '"><script>alert(1)</script>@test.com'
+E preencho os demais campos corretamente
+E seleciono a forma de pagamento "Boleto"
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo visualizar a mensagem de erro '"email" must be a valid email'
+E não devo ser redirecionado para a página de status
+```
+
+**Objetivo:** Validar que emails com caracteres especiais perigosos são rejeitados pela validação de formato.
+
+---
+
+##  Testes de Usabilidade
+
+#### **Cenário 18:** Deve permitir alternar forma de pagamento de Boleto para Cartão
+```gherkin
+Dado que estou na página de checkout
+E preencho o formulário usando Page Object
+Quando eu seleciono a forma de pagamento "Boleto"
+Então o radio button "Boleto" deve estar marcado
+Quando eu seleciono a forma de pagamento "Cartão de Crédito"
+Então o radio button "Cartão de Crédito" deve estar marcado
+E o radio button "Boleto" deve estar desmarcado
+E os campos de cartão devem aparecer
+E eu preencho os dados do cartão
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo ser redirecionado para a página de status
+```
+
+**Objetivo:** Validar que usuário pode mudar de ideia sobre forma de pagamento durante o checkout.
+
+---
+
+#### **Cenário 19:** Deve permitir alternar forma de pagamento de Cartão para Pix
+```gherkin
+Dado que estou na página de checkout
+E preencho o formulário usando Page Object
+Quando eu seleciono a forma de pagamento "Cartão de Crédito"
+Então o radio button "Cartão de Crédito" deve estar marcado
+Quando eu seleciono a forma de pagamento "Pix"
+Então o radio button "Pix" deve estar marcado
+E o radio button "Cartão de Crédito" deve estar desmarcado
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo ser redirecionado para a página de status
+```
+
+**Objetivo:** Validar transição entre formas de pagamento diferentes (cartão → Pix).
+
+---
+
+#### **Cenário 20:** Deve permitir alternar forma de pagamento de Pix para Boleto
+```gherkin
+Dado que estou na página de checkout
+E preencho o formulário usando Page Object
+Quando eu seleciono a forma de pagamento "Pix"
+Então o radio button "Pix" deve estar marcado
+Quando eu seleciono a forma de pagamento "Boleto"
+Então o radio button "Boleto" deve estar marcado
+E o radio button "Pix" deve estar desmarcado
+E aceito os termos e condições
+E clico em "Finalizar Pedido"
+Então devo ser redirecionado para a página de status
+```
+
+**Objetivo:** Validar ciclo completo de alternância entre todas as formas de pagamento disponíveis.
+
+---
+
 ##  Tecnologias Utilizadas
 
 - **Framework de Testes:** Cypress
@@ -277,10 +421,12 @@ Mas atualmente:
 
 | Categoria | Cenários | Passando | Skipados (Bugs) | Status |
 |-----------|----------|----------|-----------------|--------|
-| Cenários de Sucesso | 4 | 4 | 0 | ✅ |
+| Cenários de Sucesso | 6 | 6 | 0 | ✅ |
 | Validações de Erro - P0 | 4 | 3 | 1 | ⚠️ |
 | Validações de Erro - P1 | 4 | 2 | 2 | ⚠️ |
-| **Total** | **12** | **9** | **3** | **⚠️** |
+| Testes de Segurança | 3 | 3 | 0 | ✅ |
+| Testes de Usabilidade | 3 | 3 | 0 | ✅ |
+| **Total** | **20** | **17** | **3** | **⚠️** |
 
 ---
 
@@ -306,13 +452,15 @@ npx cypress run --spec "cypress/e2e/checkout.cy.js" --env grep="^(?!.*skip)"
 - **Page Object Model:** Implementei métodos reutilizáveis (`preencherFormularioCheckout` e `pagamentoCard`) para facilitar manutenção
 - **Priorização de testes:** Organizei cenários por criticidade (P0 e P1) baseado no impacto no negócio
 - **Checkout = fluxo crítico:** Falhas nesta funcionalidade impactam diretamente a receita
+- **Testes de segurança:** Validações contra SQL Injection e XSS demonstram preocupação com segurança da aplicação
+- **Cobertura de formas de pagamento:** Todas as 3 formas (Boleto, Cartão, Pix) foram testadas
 
 ### Limitações Conhecidas
 
-- Não foram testados cenários de **checkout como visitante** (sem criar conta)
 - Testes de **integração com gateway de pagamento real** não estão incluídos
 - **Validação de endereço via API de CEP** não foi testada
-- **Diferentes formas de pagamento** (Pix) não foram validadas
+- **Cupons de desconto** não foram validados
+- **Cálculo de frete** não foi testado
 
 ### Melhorias Futuras Recomendadas
 
@@ -320,6 +468,8 @@ npx cypress run --spec "cypress/e2e/checkout.cy.js" --env grep="^(?!.*skip)"
 - Testar **cálculo de frete** baseado no CEP
 - Adicionar testes de **timeout** em processamento de pagamento
 - Validar **persistência de dados** do formulário em caso de erro
+- Testar **validação de telefone** com formatos inválidos
+- Validar **toggle "Criar conta"** (marcar/desmarcar múltiplas vezes)
 
 ---
 
